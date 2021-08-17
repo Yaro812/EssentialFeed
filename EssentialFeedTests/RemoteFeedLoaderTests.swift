f977b6dba849c8d705cbe48f6b81307d01232b71
Yaroslav Pasternak <thorax@me.com>
2021-08-18 01:34:52 +0300

    add tests for memory leak detection

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -53,7 +53,7 @@ class RemoteFeedLoaderTests: XCTestCase {
                     let json = makeItemsJSON([])
                     client.complete(with: code, data: json, at: index)
                 }
-        }
+            }
     }
     
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
@@ -80,7 +80,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let item1 = makeItem(
             id: UUID(),
             imageURL: URL(string: "http://a-url.com")!)
-   
+        
         let item2 = makeItem(
             id: UUID(),
             description: "a description",
@@ -97,10 +97,21 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     // MARK: Helpers
     
-    func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    func makeSUT(
+        url: URL = URL(string: "https://a-url.com")!,
+        file: StaticString = #filePath,
+        line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
+        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(client)
         return (sut: sut, client: client)
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
     }
     
     private func makeItem(
